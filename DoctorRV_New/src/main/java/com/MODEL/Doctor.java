950ad8eefebe8980e89c2c381ee5b489eb60cfdb
lahcen404 <lahcen.maskour2003@gmail.com>
2025-02-26 11:51:23 +0000

    create Model of Patient

--- DoctorRV_New/src/main/java/com/MODEL/Doctor.java
+++ DoctorRV_New/src/main/java/com/MODEL/Doctor.java
@@ -5,58 +5,63 @@ public class Doctor {
     private String name;
     private String email;
     private String password;
-    private String phone;
+    private String speciality;
 
     //constructor with ID
-    public Doctor( int id , String name, String email, String password, String phone) {
+    public Doctor( int id , String name, String email, String password, String speciality) {
         this.name = name;
         this.email = email;
         this.password = password;
-        this.phone = phone;
+        this.speciality = speciality;
         this.id = id;
     }
 
     //constructor without ID
-    public Doctor(String name, String email, String password, String phone) {
+    public Doctor(String name, String email, String password, String speciality) {
         this.name = name;
         this.email = email;
         this.password = password;
-        this.phone = phone;
+        this.speciality = speciality;
     }
 
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
-    public void setName(String name){
+
+    public void setName(String name) {
         this.name = name;
     }
 
-    public String getEmail(){
+    public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email){
-        this.email=email;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    public String getPassword(){
+    public String getPassword() {
         return password;
     }
-    public void setPassword(String password){
-        this.password=password;
+
+    public void setPassword(String password) {
+        this.password = password;
     }
-    public String getPhone(){
-        return phone;
+
+    public String getSpeciality() {
+        return speciality;
     }
-    public void setPhone(String phone){
-        this.phone=phone;
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
     }
 
     @Override
@@ -66,7 +71,7 @@ public class Doctor {
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
-                ", phone='" + phone + '\'' +
+                ", phone='" + speciality + '\'' +
                 '}';
     }
 }
