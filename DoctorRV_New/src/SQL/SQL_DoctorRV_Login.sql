777c4a4b11ca256b18716793521f8748a82bbf03
lahcen404 <lahcen.maskour2003@gmail.com>
2025-02-27 00:14:55 +0000

    Correct Speciality name in SQL Script

--- DoctorRV_New/src/SQL/SQL_DoctorRV_Login.sql
+++ DoctorRV_New/src/SQL/SQL_DoctorRV_Login.sql
@@ -3,18 +3,18 @@ use DoctorDB2;
 
 CREATE TABLE patients (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
+    name VARCHAR(100) ,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL,
-    phone VARCHAR(12) NOT NULL
+    phone VARCHAR(12)
 );
 
 CREATE TABLE doctors (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
+    name VARCHAR(100) ,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL,
-    specialty VARCHAR(100) NOT NULL
+    speciality VARCHAR(100)
 );
 
 select * from patients;
